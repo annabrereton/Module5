e00e46c950ad3ef5ac84a0b93959de359a283dc2
Anna <anna.brereton@gmail.com>
2023-05-02 09:06:34 +0100

    added to finance form and console logged calculations

--- finance/src/App.js
+++ finance/src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./Components/Header";
+import Form from "./Components/Form";
+import {useState} from "react";
 
-function App() {
+const App = () => {
+
+    // here we create an array state to store the contact form data
+    const [financeDataset, updateFinanceDataset] = useState([]);
+
+    const addFinanceDataset = (financeData) => {
+        updateFinanceDataset([...financeDataset, financeData]);
+    };
+    console.log(financeDataset)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <Form addFinanceDataset={addFinanceDataset}/>
+    </>
+  )
 }
 
-export default App;
+export default App
